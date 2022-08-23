3ce30a4c8baf14a30a633046f1cd19070404092c
Chris Yates <chris.m.yates@outlook.com>
2022-08-23 00:23:04 -0400

    Move app files to 'application' - Improve README

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Title
-The worst note-taking app ever
+# _'Title'_
+
+#### A simple note-taking web application. Accessible from `master` at its [GitHub Page](https://totalchris.github.io/title).
+
+## Vision
+
+> #### _tl;dr: I'm just trying to make a good app that does what the big boys do._
+
+Create a simple note-taking app and push as many boundaries of that app as humanly possible using cutting edge web tech. Test frequently across modern rendering engines to ensure compatibility on every device. Create a MVP that would compete with Google Keep, Bundled Notes, and Apple Notes in a scenario where I care enough to do so.
+
+## Precautions
+
+> #### _tl;dr: This is a pet project made by a washed-up 22-year-old as a hobby. Use at your own risk._
+
+Using the app at this stage in development is a risk you knowingly take. The browser you use Title in stores your data. I don't see your data and therefore assume no responsibility for it.
+
+When I update Title on `master` the app may act unintentionally or clear your data. I am working on better storage methods and import/export but until then be careful. Ideally, only UI components will break update-to-update and your data will be accessible unless you clear the data yourself.
+
+## Current Features
+
+- Create plaintext notes with a title and designated color
+- Create lists of those notes, with thier own name and color
+- Rename/delete notes or lists as desired
+- Automatically save lists and their notes on exit
+
+## Flaws / To-Do
+
+- Css is spaghetti, needs sass or other preprocessor
+- Element IDs don't match current naming scheme
+- Firefox has issues rendering elements as css defines
+
+## Feature Roadmap
+
+- [x] Implement jQuery to organize element events/references
+- [x] Implement standard UI framework for cleaner look (Bootstrap)
+- [x] Add color support to notes
+- [x] Add color support to lists
+- [ ] Add icon support to lists
+- [ ] Export/Import data as file
+- [ ] Offline use support
+- [ ] Implement history api to use back + forward nav
+- [ ] Add dark mode or other themes
+- [ ] Support other types of data in notes (richtext, images, etc.)
--- title.js
+++ title.js
@@ -10,20 +10,21 @@ class Note {
 class Shelf {
   // definition of a shelf, an array collection of notes.
   constructor(name, notes, color, icon) {
-    this.notes = (notes == undefined ? [] : notes);
-    this.name = (name == undefined ? "Untitled" : name);
-    this.color = (color == undefined ? "#000000" : color);
-    this.icon = (icon == undefined ? "bi-list-ul" : icon)
+    this.notes = notes == undefined ? [] : notes;
+    this.name = name == undefined ? "Untitled" : name;
+    this.color = color == undefined ? "#000000" : color;
+    this.icon = icon == undefined ? "bi-list-ul" : icon;
     this.uuid = crypto.randomUUID();
   }
 }
 
 class App {
-  constructor(lists, activeList, activeNote){ // replaces init()
+  constructor(lists, activeList, activeNote) {
+    // replaces init()
     jQuery.fn.extend({
-      showModal: function() {
-        return this.each(function() {
-          if (this.tagName === 'DIALOG') {
+      showModal: function () {
+        return this.each(function () {
+          if (this.tagName === "DIALOG") {
             this.showModal();
           }
         });
@@ -34,112 +35,121 @@ class App {
     this.activeList = activeList;
     this.activeNote = activeNote; //only set in editing mode?
 
-    this.tool = { //buttons
-      logo : $('#logo'),
-      newList : $('#tNewShelf'),
-      newNote : $('#tNewNote'),
-      deleteNote : $('#tDelete'),
-      closeNote : $('#tClose'),
-      noteColor : $('#tNoteColor'),
-      listColor : $('#tListColor'),
-    }
+    this.tool = {
+      //buttons
+      logo: $("#logo"),
+      newList: $("#tNewShelf"),
+      newNote: $("#tNewNote"),
+      deleteNote: $("#tDelete"),
+      closeNote: $("#tClose"),
+      noteColor: $("#tNoteColor"),
+      listColor: $("#tListColor"),
+    };
 
-    this.dialog = { //dialogs
-      rename : $('#dShelfName'),
-      delete : $('#dDeleteShelf')
-    }
+    this.dialog = {
+      //dialogs
+      rename: $("#dShelfName"),
+      delete: $("#dDeleteShelf"),
+    };
 
-    this.input = { //input fields
-      listName : $('#iListName'),
-      noteName : $('#docname'),
-      noteColor : $('#doccolor'),
-      noteContent : $('#doc'),
-      listColor : $('#iListColor'),
-    }
+    this.input = {
+      //input fields
+      listName: $("#iListName"),
+      noteName: $("#docname"),
+      noteColor: $("#doccolor"),
+      noteContent: $("#doc"),
+      listColor: $("#iListColor"),
+    };
 
-    this.component =  { //UI elements, headers, paragraphs, etc.
-      deleteHeader : $('#cDeleteHeader'),
-      renameHeader : $('#cRenameHeader'),
-      listHeader : $('#tShelfListOuter'),
-      noteList : $('#list'),
-      listSelector : $('#shelfmenu'),
-    }
+    this.component = {
+      //UI elements, headers, paragraphs, etc.
+      deleteHeader: $("#cDeleteHeader"),
+      renameHeader: $("#cRenameHeader"),
+      listHeader: $("#tShelfListOuter"),
+      noteList: $("#list"),
+      listSelector: $("#shelfmenu"),
+    };
 
     // add global listener to go home on logo click
-    this.tool.logo.on('click', () => {
+    this.tool.logo.on("click", () => {
       this.viewList(this.activeList);
     });
 
     //add a tiny listener to match the color button border to the colorpicker
-    this.input.noteColor.on('input', () => {
-      this.tool.noteColor.css('border-color', this.input.noteColor.val());
+    this.input.noteColor.on("input", () => {
+      this.tool.noteColor.css("border-color", this.input.noteColor.val());
     });
 
-    this.input.listColor.on('input', () => {
-      this.tool.listColor.css('border-color', this.input.listColor.val());
-    })
+    this.input.listColor.on("input", () => {
+      this.tool.listColor.css("border-color", this.input.listColor.val());
+    });
 
     // add a global listener to add a new shelf
-    this.tool.newList.on('click', () => { 
-      this.createList(); 
+    this.tool.newList.on("click", () => {
+      this.createList();
     });
 
     // add the global listener to make a new note
-    this.tool.newNote.on('click', () => { 
-      this.createNote(); 
+    this.tool.newNote.on("click", () => {
+      this.createNote();
     });
 
     //add the global listener to close and save the active note
-    this.tool.closeNote.on('click', () => {
+    this.tool.closeNote.on("click", () => {
       this.saveNote(this.activeNote);
 
-      this.input.noteName.val('');
-      this.input.noteColor.val('#000000');
-      this.input.noteContent.val('');
+      this.input.noteName.val("");
+      this.input.noteColor.val("#000000");
+      this.input.noteContent.val("");
 
       this.viewList(this.activeList);
-    })
+    });
 
     // add the global listener to delete the active note
-    this.tool.deleteNote.on('click', () => { 
-      this.deleteNote( this.activeNote, this.activeList ); 
+    this.tool.deleteNote.on("click", () => {
+      this.deleteNote(this.activeNote, this.activeList);
 
-      this.input.noteName.val('');
-      this.input.noteColor.val('#000000');
-      this.input.noteContent.val('');
+      this.input.noteName.val("");
+      this.input.noteColor.val("#000000");
+      this.input.noteContent.val("");
 
       this.viewList(this.activeList);
     });
 
-    $('#tCancelShelfName').on('click', () => {
-      $('#fShelfName').submit();
+    $("#tCancelShelfName").on("click", () => {
+      $("#fShelfName").submit();
     });
 
-    $('#tCancelDeleteShelf').on('click', () => {
-      $('#fDeleteShelf').submit();
+    $("#tCancelDeleteShelf").on("click", () => {
+      $("#fDeleteShelf").submit();
     });
 
     // register autosave-on-close listener
-    document.addEventListener('visibilitychange', () => {
+    document.addEventListener("visibilitychange", () => {
       this.close("TitleStoredShelves");
     });
 
-    if(this.lists == undefined || this.activeList == undefined){
-      this.open('TitleStoredShelves').then(() => {this.viewList(this.lists[0]);});
+    if (this.lists == undefined || this.activeList == undefined) {
+      this.open("TitleStoredShelves").then(() => {
+        this.viewList(this.lists[0]);
+      });
     }
   }
 
-  async open(key){
+  async open(key) {
     this.lists = [];
     let retrieved = JSON.parse(window.localStorage.getItem(key));
     if (retrieved == null || retrieved.length == 0) {
-      retrieved = await this.importData('./demo.json');
+      retrieved = await this.importData("./dep/application/demo.json");
     }
-    retrieved.forEach((l) => { this.lists.push(l) });
+    retrieved.forEach((l) => {
+      this.lists.push(l);
+    });
 
-    this.component.listSelector.html('');
-    this.lists.forEach((l) => { 
-      this.component.listSelector.append($(`
+    this.component.listSelector.html("");
+    this.lists.forEach((l) => {
+      this.component.listSelector.append(
+        $(`
       <li class="notelist-item" uuid="${l.uuid}">
         <button class="dropdown-item shelf-name" id="${l.uuid}" onclick="Title.viewList(Title.lists.find((s) => { return s.uuid == '${l.uuid}' }))">
           <div style="color: ${l.color};">${l.name}<div>
@@ -147,34 +157,37 @@ class App {
           <button class="dropdown-item shelf-option text-danger shelfop-delete"><i class="bi bi-trash-fill" onclick="Title.deleteList(Title.lists.find((s) => { return s.uuid == '${l.uuid}' }))"></i></button>
         </button>
       </li>
-      `));
+      `)
+      );
     });
 
     if (this.lists.length <= 1) {
-      $('.shelfop-delete').css('display', 'none');
+      $(".shelfop-delete").css("display", "none");
     } else {
-      $('.shelfop-delete').css('display', 'inline-block');
+      $(".shelfop-delete").css("display", "inline-block");
     }
   }
 
-  async importData(uri){
+  async importData(uri) {
     let raw = await fetch(uri);
     return await raw.json();
   }
 
-  createList(){
-    this.dialog.rename.one('close', () => {
-      if (this.dialog.rename[0].returnValue == 'yes') {
-
-        let len = this.lists.push(new Shelf(this.input.listName.val(), [], this.input.listColor.val()));
+  createList() {
+    this.dialog.rename.one("close", () => {
+      if (this.dialog.rename[0].returnValue == "yes") {
+        let len = this.lists.push(
+          new Shelf(this.input.listName.val(), [], this.input.listColor.val())
+        );
         let l = this.lists[len - 1];
         this.viewList(l);
 
-        this.component.renameHeader.html('');
-        this.input.listName.val('');
-        this.input.listColor.val('#000000');
+        this.component.renameHeader.html("");
+        this.input.listName.val("");
+        this.input.listColor.val("#000000");
 
-        this.component.listSelector.append($(`
+        this.component.listSelector.append(
+          $(`
         <li class="notelist-item" uuid="${l.uuid}">
           <button class="dropdown-item shelf-name" id="${l.uuid}" onclick="Title.viewList(Title.lists.find((s) => { return s.uuid == '${l.uuid}' }))">
             <div style="color: ${l.color};">${l.name}<div>
@@ -182,65 +195,68 @@ class App {
             <button class="dropdown-item shelf-option text-danger shelfop-delete"><i class="bi bi-trash-fill" onclick="Title.deleteList(Title.lists.find((s) => { return s.uuid == '${l.uuid}' }))"></i></button>
           </button>
         </li>
-        `));
+        `)
+        );
 
         if (this.lists.length <= 1) {
-          $('.shelfop-delete').css('display', 'none');
+          $(".shelfop-delete").css("display", "none");
         } else {
-          $('.shelfop-delete').css('display', 'inline-block');
+          $(".shelfop-delete").css("display", "inline-block");
         }
       }
     });
 
-    this.input.listName.val('');
-    this.input.listColor.val('#000000');
-    this.tool.listColor.css('border-color', '#000000');
-    this.component.renameHeader.html('New List');
+    this.input.listName.val("");
+    this.input.listColor.val("#000000");
+    this.tool.listColor.css("border-color", "#000000");
+    this.component.renameHeader.html("New List");
 
     this.dialog.rename.showModal();
   }
 
-  renameList(list){
-    this.dialog.rename.one('close', () => {
-      if (this.dialog.rename[0].returnValue == 'yes') {
-
+  renameList(list) {
+    this.dialog.rename.one("close", () => {
+      if (this.dialog.rename[0].returnValue == "yes") {
         list.name = this.input.listName.val();
         list.color = this.input.listColor.val();
         this.viewList(list);
 
-        this.component.renameHeader.html('');
-        this.input.listName.val('');
-        this.input.listColor.val('#000000');
-        this.tool.listColor.css('border-color', '#000000');
+        this.component.renameHeader.html("");
+        this.input.listName.val("");
+        this.input.listColor.val("#000000");
+        this.tool.listColor.css("border-color", "#000000");
 
-        $(`li.notelist-item[uuid=${list.uuid}] .shelf-name div`).html(list.name);
-        $(`li.notelist-item[uuid=${list.uuid}] .shelf-name div`).css('color', list.color);
-
+        $(`li.notelist-item[uuid=${list.uuid}] .shelf-name div`).html(
+          list.name
+        );
+        $(`li.notelist-item[uuid=${list.uuid}] .shelf-name div`).css(
+          "color",
+          list.color
+        );
       }
     });
 
     this.input.listName.val(list.name);
     this.input.listColor.val(list.color);
-    this.tool.listColor.css('border-color', list.color);
-    this.component.renameHeader.html('Edit List');
+    this.tool.listColor.css("border-color", list.color);
+    this.component.renameHeader.html("Edit List");
 
     this.dialog.rename.showModal();
   }
 
-  deleteList(list){
-    this.dialog.delete.one('close', () => {
-      if (this.dialog.delete[0].returnValue == 'yes') {
-
+  deleteList(list) {
+    this.dialog.delete.one("close", () => {
+      if (this.dialog.delete[0].returnValue == "yes") {
         this.lists.splice(this.lists.indexOf(list), 1);
         this.viewList(this.lists[this.lists.length - 1]);
 
-        this.component.deleteHeader.html('');
+        this.component.deleteHeader.html("");
 
         $(`li.notelist-item[uuid=${list.uuid}]`).remove();
         if (this.lists.length <= 1) {
-          $('.shelfop-delete').css('display', 'none');
+          $(".shelfop-delete").css("display", "none");
         } else {
-          $('.shelfop-delete').css('display', 'inline-block');
+          $(".shelfop-delete").css("display", "inline-block");
         }
       }
     });
@@ -250,93 +266,118 @@ class App {
     this.dialog.delete.showModal();
   }
 
-  createNote(list){
-    let l = (list == undefined ? this.activeList : list)
-    let len = l.notes.push(new Note('', '', l.color));
-    let n = l.notes[len - 1]
+  createNote(list) {
+    let l = list == undefined ? this.activeList : list;
+    let len = l.notes.push(new Note("", "", l.color));
+    let n = l.notes[len - 1];
     this.viewNote(n);
     $(`
-    <div class="card notecard" uuid="${n.uuid}" style="color: ${n.color}; border-color: ${n.color};">
+    <div class="card notecard" uuid="${n.uuid}" style="color: ${
+      n.color
+    }; border-color: ${n.color};">
     <div class="card-body">
-      <h5 class="card-title">${n.name == '' ? 'Untitled Note' : n.name}</h5>
+      <h5 class="card-title">${n.name == "" ? "Untitled Note" : n.name}</h5>
       <div class="card-text">${n.content}</div>
       </div>
     </div> 
-    `).on('click', (e) => {this.viewNote(n);}).appendTo(this.component.noteList);
-    if(l.notes.length >= 1){
-      $('#cEmptyListHeader').remove();
+    `)
+      .on("click", (e) => {
+        this.viewNote(n);
+      })
+      .appendTo(this.component.noteList);
+    if (l.notes.length >= 1) {
+      $("#cEmptyListHeader").remove();
     }
   }
 
-  saveNote(note){
+  saveNote(note) {
     note.name = Title.input.noteName.val();
     note.color = Title.input.noteColor.val();
     note.content = Title.input.noteContent.val();
-        //ToDo: alter physical component
-    $(`.notecard[uuid=${note.uuid}]`).attr("style", `color: ${note.color}; border-color: ${note.color};`);
-    $(`.notecard[uuid=${note.uuid}] h5.card-title`).html((note.name == '' ? 'Untitled Note' : note.name));
+    //ToDo: alter physical component
+    $(`.notecard[uuid=${note.uuid}]`).attr(
+      "style",
+      `color: ${note.color}; border-color: ${note.color};`
+    );
+    $(`.notecard[uuid=${note.uuid}] h5.card-title`).html(
+      note.name == "" ? "Untitled Note" : note.name
+    );
     $(`.notecard[uuid=${note.uuid}] div.card-text`).html(note.content);
   }
 
-  deleteNote(note, list){
-    list = (list == undefined ? Title.activeList : list); //use the active list if none defined
-    if(note != undefined){
+  deleteNote(note, list) {
+    list = list == undefined ? Title.activeList : list; //use the active list if none defined
+    if (note != undefined) {
       list.notes.splice(list.notes.indexOf(note), 1);
-      Title.activeNote = (Title.activeNote == note ? undefined : Title.activeNote); //unset active note if its the same note
+      Title.activeNote =
+        Title.activeNote == note ? undefined : Title.activeNote; //unset active note if its the same note
     }
-    $(`.notecard[uuid=${note.uuid}]`).remove()
-    if(list.notes.length <= 0){
-      $(`<p id="cEmptyListHeader">No Notes, click <i class="bi bi-file-earmark-plus"></i> to add one!</p>`).appendTo(this.component.noteList);
+    $(`.notecard[uuid=${note.uuid}]`).remove();
+    if (list.notes.length <= 0) {
+      $(
+        `<p id="cEmptyListHeader">No Notes, click <i class="bi bi-file-earmark-plus"></i> to add one!</p>`
+      ).appendTo(this.component.noteList);
     }
-
   }
 
-  viewList(list){
-    $('body').attr('activeView', 'list');
+  viewList(list) {
+    $("body").attr("activeView", "list");
     this.activeNote = undefined;
-    document.title = (list.name == undefined ? 'Untitled List' : list.name);
-    this.component.listHeader.html(list.name == undefined ? 'Untitled List' : list.name);
-    this.component.listHeader.css('color', list.color);
-    this.component.listHeader.css('border-color', list.color);
+    document.title = list.name == undefined ? "Untitled List" : list.name;
+    this.component.listHeader.html(
+      list.name == undefined ? "Untitled List" : list.name
+    );
+    this.component.listHeader.css("color", list.color);
+    this.component.listHeader.css("border-color", list.color);
 
-    if(this.activeList == undefined || list != this.activeList){
+    if (this.activeList == undefined || list != this.activeList) {
       this.activeList = list;
-      this.component.noteList.html('');
-  
+      this.component.noteList.html("");
+
       if (list.notes.length == 0) {
-        $(`<p id="cEmptyListHeader">No Notes, click <i class="bi bi-file-earmark-plus"></i> to add one!</p>`).appendTo(this.component.noteList);
+        $(
+          `<p id="cEmptyListHeader">No Notes, click <i class="bi bi-file-earmark-plus"></i> to add one!</p>`
+        ).appendTo(this.component.noteList);
       } else {
         list.notes.forEach((note) => {
           $(`
-          <div class="card notecard" uuid="${note.uuid}" style="color: ${note.color}; border-color: ${note.color};">
+          <div class="card notecard" uuid="${note.uuid}" style="color: ${
+            note.color
+          }; border-color: ${note.color};">
           <div class="card-body">
-            <h5 class="card-title">${note.name == '' ? 'Untitled Note' : note.name}</h5>
+            <h5 class="card-title">${
+              note.name == "" ? "Untitled Note" : note.name
+            }</h5>
             <div class="card-text">${note.content}</div>
             </div>
           </div> 
-          `).on('click', (e) => {this.viewNote(note);}).appendTo(this.component.noteList);
+          `)
+            .on("click", (e) => {
+              this.viewNote(note);
+            })
+            .appendTo(this.component.noteList);
         });
       }
     }
   }
 
-  viewNote(note){
-    $('body').attr('activeView', 'edit');
+  viewNote(note) {
+    $("body").attr("activeView", "edit");
     Title.activeNote = note;
 
-    this.input.noteName.val(note.name == undefined ? '' : note.name);
-    this.input.noteContent.val(note.content == undefined ? '' : note.content);
-    this.input.noteColor.val(note.color == undefined ? '#000000' : note.color);
-    this.tool.noteColor.css('border-color', $('#doccolor').val());
-    document.title = note.name == undefined ? 'Untitled Note' : note.name;
+    this.input.noteName.val(note.name == undefined ? "" : note.name);
+    this.input.noteContent.val(note.content == undefined ? "" : note.content);
+    this.input.noteColor.val(note.color == undefined ? "#000000" : note.color);
+    this.tool.noteColor.css("border-color", this.input.noteColor.val());
+    document.title = note.name == undefined ? "Untitled Note" : note.name;
   }
 
-  close(key){
+  close(key) {
     window.localStorage.setItem(key, JSON.stringify(this.lists));
     //any other closing ops
   }
 }
 
-function init(){
+function init() {
   this.Title = new App();
 }
